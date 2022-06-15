fa25de97137b07a461e3db66fe88a3c3422e1732
Naman <89750281+nagfx@users.noreply.github.com>
2022-06-16 02:42:06 +0800

    Create module.py
    Just showing I have module knowledge.

---
+++ module.py
@@ -0,0 +1,11 @@
+# Displaying modules functionality by Naman
+
+
+# importing the math module, it got features for mathematics related work
+import math
+
+# the math module possesses heaps of pre-built functions
+print("The square root of 81 is", math.sqrt(81))
+
+# Listing the value of Pi.
+print("The value of the Pi is:", math.pi)
