d268bb0a3fc7d25eb959017c02562af6d83c1694
Naman <89750281+nagfx@users.noreply.github.com>
2022-06-16 02:35:39 +0800

    Create hello.py
    Just demonstrating I do know how to print hello world.

---
+++ hello.py
@@ -0,0 +1,15 @@
+# The classic usual Hello World app formality by Naman
+
+print("Hello World")
+
+# I could also just store it into a variable
+hello = "Hello World -by"
+name = "Naman"
+name1 = " Naman"
+# I could combine both which will concatenate
+hel = hello+name
+print(hello, name1)
+
+print("Now")
+
+print(hel)
